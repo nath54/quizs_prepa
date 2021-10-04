76e9c567d1c882ffe94b650e0efbc38ba105619e
nath54 <cerisara.nathan@gmail.com>
2021-10-04 18:25:45 +0200

    travaille sur les maths

---
+++ maths/data_quizs.js
@@ -0,0 +1,60 @@
+const quiz_logique = [{
+        "type": "définition",
+        "titre": "Formule propositionnelle",
+        "resultat": "Une formule propositionnelle est une liant des propositions élémentaires représentées par des lettres (ou variables propositionnelles), à l'aide d'un certain nombre de symboles représentant des opérations logiques."
+    },
+    {
+        "type": "définition",
+        "titre": "Formules équivalentes",
+        "resultat": "Deux formules A et B sont dites équivalentes si elles prennent la même valeur de vérité l'une et l'autre, quelle que soit la distribution de vérités données sur l'ensemble des variables propositionnelles intervenant dans ces formules. Autrement dit, elles sont vraies et fausses sous les mêmes conditions sur les variables propositionnelles."
+    },
+    {
+        "type": "définition",
+        "titre": "Tautologie",
+        "resultat": "Ce sont des formules toujours vraies (pour toute distribution de vérité)."
+    },
+    {
+        "type": "proposition",
+        "titre": "Associativité du $ou",
+        "hypotheses": "A, B et C désignent des variables propositionnelles",
+        "resultat": "(A $ou B) $ou C $equivalent A $ou (b $ou C)"
+    },
+    {
+        "type": "proposition",
+        "titre": "Associativité du $et",
+        "hypotheses": "A, B et C désignent des variables propositionnelles",
+        "resultat": "(A $et B) $et C $equivalent A $et (B $et C)"
+    },
+    {
+        "type": "proposition",
+        "titre": "Distributivité du $ou",
+        "hypotheses": "A, B et C désignent des variables propositionnelles",
+        "resultat": "A $et(B $ou C) $equivalent(A $et B) $ou(A $et C)"
+    },
+    {
+        "type": "proposition",
+        "titre": "Distributé du $et",
+        "hypotheses": "A, B et C désignent des variables propositionnelles",
+        "resultat": " A $ou(B $et C) $equivalent(A $ou B) $et(A $ou C)"
+    },
+    {
+        "type": "proposition",
+        "titre": "Equivalence de l'impliquation",
+        "hypotheses": "A, B et C désignent des variables propositionnelles",
+        "resultat": "(A $implique B) $equivalent (B $ou $non A)"
+    },
+    {
+        "type": "proposition",
+        "titre": "Contraposée",
+        "hypotheses": "A, B et C désignent des variables propositionnelles",
+        "resultat": "(A $implique B) $equivalent ($non B $implique $non A)"
+    }
+];
+
+
+
+// TOUS LES QUIZS SONT DANS LA VARIABLE QUIZS
+
+const Quizs = {
+    "logique": quiz_logique,
+};
---
+++ maths/index.js
@@ -0,0 +1,94 @@
+window.themes_select = {
+
+};
+
+function init() {
+    for (theme of Object.keys(Quizs)) {
+        var bt = document.createElement("a");
+        bt.innerHTML = theme;
+        bt.classList.add("btn");
+        bt.classList.add("btn-outline-info");
+        bt.classList.add("m-1");
+        bt.setAttribute("onclick", "toggle_theme('" + theme + "');");
+        bt.id = "button_" + theme;
+        document.getElementById("liste_themes").appendChild(bt);
+        window.themes_select[theme] = false;
+    }
+}
+
+function toggle_theme(theme) {
+    if (window.themes_select[theme]) {
+        window.themes_select[theme] = false;
+        document.getElementById("button_" + theme).setAttribute("class", "btn btn-outline-info m-1");
+    } else {
+        window.themes_select[theme] = true;
+        document.getElementById("button_" + theme).setAttribute("class", "btn btn-info m-1");
+    }
+}
+
+function select_all() {
+    var tout_selectionne = true;
+    for (theme of Object.keys(window.themes_select)) {
+        if (!window.themes_select[theme]) {
+            tout_selectionne = false;
+            break;
+        }
+    }
+    //
+    if (tout_selectionne) {
+        for (theme of Object.keys(window.themes_select)) {
+            toggle_theme(theme);
+        }
+    } else {
+        for (theme of Object.keys(window.themes_select)) {
+            if (!window.themes_select[theme]) {
+                toggle_theme(theme);
+            }
+        }
+    }
+}
+
+function launch() {
+    var url = "quiz.html?themes=";
+    var nb_themes = 0;
+    //
+    for (theme of Object.keys(window.themes_select)) {
+        if (window.themes_select[theme]) {
+            if (nb_themes > 0) {
+                url += ",";
+            }
+            url += theme;
+            nb_themes++;
+        }
+    }
+    //
+    if (nb_themes == 0) {
+        alert("Il faut avoir au moins selectionné un theme !");
+        return;
+    }
+    //
+    window.location.href = url;
+}
+
+
+function launch_liste_mots() {
+    var url = "liste_mots.html?themes=";
+    var nb_themes = 0;
+    //
+    for (theme of Object.keys(window.themes_select)) {
+        if (window.themes_select[theme]) {
+            if (nb_themes > 0) {
+                url += ",";
+            }
+            url += theme;
+            nb_themes++;
+        }
+    }
+    //
+    if (nb_themes == 0) {
+        alert("Il faut avoir au moins selectionné un theme !");
+        return;
+    }
+    //
+    window.location.href = url;
+}
