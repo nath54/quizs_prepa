06358dd1ed91ab8f5975a6131ef2f17598e0447c
nathan cerisara <cerisara.nathan@gmail.com>
2022-04-18 14:10:50 +0100

    aa

--- agl/data_quizs.js
+++ agl/data_quizs.js
@@ -2262,6 +2262,177 @@ const quiz_globalization = [
     ["griefs", ["complaints", "objections"], 1612],
 ];
 
+const quiz_immigration = [
+    ["A foreigner ", "Un étranger", 1492],
+    ["A stranger ", "Un inconnu", 1493],
+    ["Nationality ", "La nationalité", 1494],
+    ["A foreign national ", "Un ressortissant étranger", 1495],
+    ["French citizens ", "Les personnes de nationalité française", 1496],
+    ["To emigrate to New Zealand ", "Émigrer en Nouvelle-Zélande", 1497],
+    ["To immigrate to France ", "Immigrer en France", 1498],
+    ["A wave of immigration ", "Une vague d'immigration", 1499],
+    [
+        ["Large-scale immigration", "mass immigration"],
+        ["Immigration à grande échelle", "immigration de masse"], 1500
+    ],
+    ["An exodus ", "Un exode", 1501],
+    ["A newcomer ", "Un nouvel arrivant", 1502],
+    [
+        ["Native country", " home country "],
+        ["Pays natal", " pays d'origine"], 1503
+    ],
+    ["Homeland ", "La patrie", 1504],
+    ["A host country ", "Un pays d'accueil", 1505],
+    [
+        ["An adoptive country", " a country of adoption "], "Un pays d'adoption", 1506
+    ],
+    ["To welcome ", "Accueillir", 1507],
+    ["Welcoming ", "Accueillant", 1508],
+    ["Immigration policy ", "La politique migratoire", 1509],
+    [
+        ["Selective immigration", " controlled immigration "],
+        ["Immigration choisie", " immigration contrôlée"], 1510
+    ],
+    ["Migration flows ", "Les flux migratoires", 1511],
+    ["A stowaway ", "Un passager clandestin", 1512],
+    ["Illegal immigration ", ["L'immigration clandestine", " irrégulière"], 1513],
+    ["Uncontrolled immigration ", "L'immigration sauvage", 1514],
+    [
+        ["An illegal immigrant", " an illegal alien", " an undocumented alien "],
+        ["Un immigrant clandestin", "un sans-papiers"], 1515
+    ],
+    ["His papers are not in order ", "Il est en situation irrégulière", 1516],
+    [
+        ["A border", " a frontier "], "Une frontière", 1517
+    ],
+    ["To close one's border ", "Fermer ses frontières", 1518],
+    ["Border controls ", "Les contrôles aux frontières", 1519],
+    ["Identity checks ", "Les contrôles d'identité", 1520],
+    [
+        ["A holding centre", " a detention facility "], "Un centre de rétention (administrative)", 1521
+    ],
+    ["A passport ", "Un passeport", 1522],
+    ["An identity card ", "Une carte d'identité", 1523],
+    ["A visa ", "Un visa", 1524],
+    ["A refugee ", "Un réfugié", 1525],
+    ["A displaced person ", "Une personne déplacée", 1526],
+    ["A stateless person ", "Un apatride", 1527],
+    [
+        ["To take refuge", " take shelter "], "Se réfugier", 1528
+    ],
+    ["To seek political asylum ", "Demander l'asile politique", 1529],
+    ["An asylum-seeker ", "Un demandeur d'asile", 1530],
+    ["To flee persecution / destitution ", "Fuir la persécution / la misère", 1531],
+    ["To escape poverty ", "Échapper à la pauvreté", 1532],
+    ["To be in search of better living-conditions ", "Chercher de meilleures conditions de vie", 1533],
+    ["Human trafficking ", "Le trafic des êtres humains", 1534],
+    ["A smuggler ", ["Un contrebandier", " un passeur"], 1535],
+    ["People-smugglers ", "Les passeurs d'immigrants clandestins", 1536],
+    ["A smuggling ring ", "Un réseau de passeurs", 1537],
+    ["A fake passport ", "Un faux passeport", 1538],
+    ["To escort sb back to the border ", "Reconduire qqn à la frontière", 1539],
+    ["To send an immigrant back to his native country ", "Renvoyer un immigrant dans son pays d'origine", 1540],
+    ["To deport sb ", "Expulser qqn", 1541],
+    ["To be turned away at the border ", "Être refoulé à la frontière", 1542],
+    ["To be ordered out of the country ", "Être expulsé", 1543],
+    ["To go into exile ", "S'exiler", 1544],
+    ["An exile ", "Un exilé", 1545],
+    [
+        ["To exile", " banish sb "],
+        ["Exiler", " bannir qqn"], 1546
+    ],
+    ["To expatriate oneself ", "S'expatrier", 1547],
+    [
+        ["An expatriate", " an expat "], "Un expatrié", 1548
+    ],
+    ["To defect to the West / to the ennemy ", "Passer à l'Ouest / à l'ennemi", 1549],
+    ["Brain drain / brain gain ", "Fuite des cerveaux / afflux des cerveaux", 1550],
+    ["To start from scratch ", "Partir de rien", 1551],
+    ["Moonlighting ", "Le travail au noir", 1552],
+    ["A sweatshop ", "Un atelier clandestin", 1553],
+    ["To exploit migrant workers ", "Exploiter les travailleurs immigrés", 1554],
+    ["A work permit ", ["Un permis de travail", " une carte de travail"], 1555],
+    [
+        ["A residence permit", " a resident permit "],
+        ["Un permis de séjour", " une carte de séjour"], 1556
+    ],
+    ["A migrant worker ", "Un travailleur migrant", 1557],
+    ["A guest worker ", "Un travailleur invité (disposant d'un visa temporaire)", 1558],
+    ["To become integrated ", "S'intégrer", 1559],
+    ["To integrate well into a society ", "Bien s'intégrer dans une société", 1560],
+    ["Social integration ", "L'insertion sociale", 1561],
+    ["To regularise the status of illegal immigrants ", "Régulariser la situation des clandestins", 1562],
+    ["Regularisation ", "La régularisation", 1563],
+    [
+        ["To be granted British citizenship", " to become a British citizen "],
+        ["Se faire naturaliser britannique", " être naturalisé britannique", " obtenir la nationalité britannique"], 1564
+    ],
+    ["A quota ", "Un quota", 1565],
+    [
+        ["Family reunion", " reunification "], "Le regroupement familial", 1566
+    ],
+    ["A benefit cheat ", "Qqn qui abuse du système des prestations sociales", 1567],
+    ["Benefit fraud ", "La fraude aux prestations sociales", 1568],
+    ["A freeloader ", "Un parasite", 1569],
+    [
+        ["A sham marriage", " a marriage of convenience "], "Un mariage blanc", 1570
+    ],
+    ["To flood ", "Inonder", 1571],
+    ["To be swamped with foreigners ", "Être envahi par les étrangers", 1572],
+    ["The influx of workers ", ["L'arrivée massive", " l'afflux de travailleurs"], 1573],
+    ["To stem the flow of immigrants ", "Endiguer l'afflux d'immigrés", 1574],
+    [
+        ["To restrict immigration", "to curb immigration"],
+        ["Limiter", "freiner l'immigration"], 1575
+    ],
+    ["A far-right party ", "Un parti d'extrême droite", 1576],
+    ["To scare immigrants away ", "Faire fuir les immigrants", 1577],
+    ["To keep out indesirables ", "Empêcher les indésirables d'entrer", 1578],
+    ["Xenophobia ", "La xénophobie", 1579],
+    ["Xenophobic ", "Xénophobe", 1580],
+    ["Second-generation immigrants ", "Des immigrés de deuxième génération", 1581],
+    ["To have a foreign-born grandfather ", "Avoir un grand-père né à l'étranger", 1582],
+    ["Young men of Asian / Portuguese descent ", "Des jeunes gens d'origine asiatique / portugaise", 1583],
+    ["A native of France ", "Un Français de naissance", 1584],
+    ["A born and bred New Yorker ", "Un New Yorkais de souche", 1585],
+    [
+        ["He's of French origin", " of French extraction "], "Il est français de souche", 1586
+    ],
+    ["North Africa ", "Le Maghreb", 1587],
+    ["North Africans ", "Les Maghrébins", 1588],
+    ["Pakistani / Bangladeshi ", "Pakistanais / du Bangladesh", 1589],
+    ["Spanish-speaking / Hindi-speaking ", "Hispanophone / parlant le Hindi", 1590],
+    ["Diaspora ", "Diaspora", 1591],
+    ["Un fléau ", "A scourge", 1592],
+    ["uproot ", "se déraciner", 1593],
+    ["beggar ", "mendiant", 1594],
+    ["Foreignness ", "extranéité", 1595],
+    ["multiculturalism ", "communautarisme", 1596],
+    ["which states ", "qui affirme", 1597],
+    ["proponent ", "un partisan", 1598],
+    ["conspiracy theory ", "théorie du complot", 1599],
+    ["welfare ", "bien-être", 1600],
+    ["magnaminous ", "généreux", 1601],
+    ["Openness ", "ouverture", 1602],
+    ["sweeping ", "balayant", 1603],
+    ["On purely economic grounds ", "Pour des raisons purement économiques", 1604],
+    ["Attributes ", "attributs", 1605],
+    ["hardheaded ", "réaliste", 1606],
+    ["green card ", "un permis de séjour", 1607],
+    ["relief ", ["soulagement", " aide"], 1608],
+    [
+        ["A lawsuit", "a trial"], "procès", 1609
+    ],
+    ["willingness ", "volonté", 1610],
+    ["bill ", "un projet de loi", 1611],
+    ["fast-tracked ", "accéléré", 1612],
+    ["crime ", "un délit", 1613],
+    ["to cope ", ["faire face", " s'en sortir"], 1614],
+    ["thus ", ["Par conséquent", " ainsi", " de cette façon"], 1615],
+    ["alleviate ", "Alléger", 1616],
+    ["resettle ", "réinstaller", 1617],
+    ["to a large extent ", "en grande partie", 1618],
+];
 // NEXT FREE ID : 1613
 
 
@@ -2284,6 +2455,7 @@ const Quizs = {
     "solidarity": quiz_solidarity,
     "the media": quiz_media,
     "rich-poor": quiz_rich_poors,
+    "immigration": quiz_immigration,
     "globalization": quiz_globalization,
     "vocabulaire - réflexion": quizs_voc_reflexion,
     "vocabulaire - débat": quizs_voc_debat,
